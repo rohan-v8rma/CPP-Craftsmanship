a3b852d8f9bb811036976d25cbc7e45b4b106781
rohan-verma19 <rv725555@gmail.com>
2022-02-25 15:19:12 +0530

    feat: Added arrays in C explanation

---
+++ 08-arrays-in-C.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+/*
+        INDEX   
+1. Arrays in C
+2. Static Allocation
+TODO 3. Dynamic Allocation
+*/
+
+int main () {
+/*
+* 1. Arrays in C
+Arrays in C can store homogenous data like only
+The numpy API in python uses C arrays which is why it is so fast.
+
+? SYNTAX of Declaring an array:
+    <datatype-of-stored-elements> <array-name>[<number-of-elements>];
+? SYNTAX of Initializing an array:
+    <datatype-of-stored-elements> <array-name>[<number-of-elements>] = {<array>};
+
+! NOTE: When we assign a value to a variable while declaring it, it is called INITIALIZATION.
+! If we declare it first and assign a value to it later, it is called ASSIGNMENT.
+
+* 2. Static Allocation
+
+Static Allocation is when we enter a numerical value directly
+indicating the length of the array we are declaring. 
+? For example:
+?     int arr[30];
+Here, we are directly specifying a numerical value as the length of the array
+
+
+*/
+/*
+TODO: 3. Dynamic Allocation
+
+When we use pointers and dynamically allocate space for our array, it is called
+Dynamic Allocation.
+*/
+    return 0;
+}
