aec240cb2a57ead2acd72514c1e73ffbee9c696a
rohan-verma19 <rv725555@gmail.com>
2022-02-25 17:52:41 +0530

    improv: Cleaned up logic

--- basic-programs/02-integer-digit-sum.c
+++ basic-programs/02-integer-digit-sum.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int integer = 234325;
-    int digit;
-    int sum = 0;
-    int higher_digit = -1;
-    int divisor = 1;
-    while (higher_digit != 0) {
-        digit = ((integer % (10 * divisor)) / divisor);
-        sum += digit;
-        integer -= divisor * digit;
-        divisor *= 10;
-        higher_digit = integer / divisor;
-    }
-    printf("Sum of the digits is %d\n", sum);
+    int integer, digit;
+    int sum_of_digits = 0;
+    
+    scanf("%d", &integer);
+
+    while (integer != 0) {
+        digit = integer % 10; //gives current unit digit of integer
+        sum_of_digits += digit; //adds the digits to the sum
+        integer /= 10; //makes the tens digit the units digit by performing floor division
+        //repeat
+    };
+    printf("Sum of the digits is %d\n", sum_of_digits);
     return 0;
 }
