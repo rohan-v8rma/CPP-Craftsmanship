3cd1d1ceb433d1f3c66f7c35134979b8cbb7c680
rohan-verma19 <rv725555@gmail.com>
2022-03-01 07:29:29 +0530

    improv: Added explanation of overflow in char

--- 06-extendedASCII.c
+++ 06-extendedASCII.c
@@ -3,8 +3,18 @@
 int main() {
     /*
     Most C compilers implement an extended version of ASCII which
-    has 256 characters instead of 
+    has 256 characters instead of 129
     */
+    char num = 'b'+'b'+'b';
+    /* 
+    this will throw error as we are assigning a 294 ascii value to 'char' data type
+    which will give an overflow error. 
+    Till 255, ASCII is as-is. 
+    After that 256 is considered as ASCII value 0. 
+    ! NOTE: The problem is not in the operation rather the assignment itself.
+    ! Which is why if we assign this expression to `integer` data type, there will be no error. 
+    */
+    int new = 'b'+'b'+'b';
 
     return "0";
 }
