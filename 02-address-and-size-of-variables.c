37550ea8722287c6b35fceacb06880dce1a4a846
rohan-verma19 <rv725555@gmail.com>
2022-03-01 07:28:24 +0530

    feat: sizeof() operator explanation

--- 02-address-and-size-of-variables.c
+++ 02-address-and-size-of-variables.c
@@ -3,7 +3,7 @@
 /*
             INDEX
 1. 'Address of' (&) operator
-2. sizeof() function
+2. sizeof() operator
 */
 
 
@@ -17,13 +17,29 @@ The variable doesn't even have to be initialized, just declared.
 */
     int a;
     scanf("%d", &a);
-    printf("%d", a);
-    // printf('\n');
-    return 0;
+    printf("%d\n", a);
+        
 /*
-* 2. sizeof() function
+* 2. sizeof() operator
 
-The
+The sizeof operator is the most common operator in C. 
+
+It is a compile-time unary operator and used to compute the size of its operand. 
+It returns the size of a variable. It can be applied to any data type, 
+float type, pointer type variables.
+
+When sizeof() is used with the data types, it simply returns the amount of 
+memory allocated to that data type.
+
+! NOTE: In order to print the size of any variable, we need to give %ld (long int)
+! or %lu (unsigned long int) format specifier, otherwise, we will encounter error.
 */
 
+    printf("Size of variable a : %ld\n", sizeof(a));
+    printf("Size of int data type : %ld\n", sizeof(int));
+    printf("Size of char data type : %ld\n", sizeof(char));
+    printf("Size of float data type : %ld\n", sizeof(float));
+    printf("Size of double data type : %ld\n", sizeof(double));
+    
+    return 0;
 }
