7f7159d1d04b56b240c7adfdfe4d8ba1961f58d0
rohan-verma19 <rv725555@gmail.com>
2022-03-15 08:07:56 +0530

    improv: relocated information about scoping to 09B

--- 04B-Variables.cpp
+++ 04B-Variables.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 /*
             INDEX  
-1. About variables
+1. What are variables?
 2. Different ways of declaring variables
 
+TODO see 09B for more information about variable scopes
 */
 int main(){
 
-//! 1. About variables
-
+//* 1. What are variables?
 
 /*
 Variables represent named storage locations, whose value
@@ -18,14 +18,14 @@ can be manipulated during the program run.
 Variables declared outside functions in the body of the program
 are global variables and can be accessed anywhere within the program.
 
-* Variables declared within the body of functions are local to that 
-* specific function. They can be accessed only within that function.
+? Variables declared within the body of functions are local to that 
+? specific function. They can be accessed only within that function.
 
 ! Official definition of C++ rules regarding variables: A variable can be accessed 
 ! only in the block in which it has been declared into(local to that block).
 
-* A variable declared inside the block of main() and not in any sub block can be 
-* accessed anywhere inside main() i.e., the scope of the variable is the function main().
+? A variable declared inside the block of main() and not in any sub block can be 
+? accessed anywhere inside main() i.e., the scope of the variable is the function main().
 
 There are two values associated with a variable :
 
@@ -49,9 +49,7 @@ E.g:- sum = 100, the left one is lvalue and the right one is rvalue.
 */
 
 
-
-//! 2. Different ways of declaring variables
-
+//* 2. Different ways of declaring variables
 
 /*
 ! SYNTAX : [typeModifier] dataType variableName;
@@ -92,21 +90,6 @@ Example:
     float avg = val1/val;
 
 /*
-! Scope of iteration variable & variables declared within loop and if-else statements.
-
-Earlier, the same variable could not be declared in another loop as the iteration variable.
-
-Now, the iteration variable as well as the variables declared within the loopBody or the body
-of if or else statements, cannot be accessed after their scope is over 
-OR 
-outside the body-of-if or loopBody. 
-
-?    for (char ch = 'a'; ch <= 'z'; ++ch); //ch declared & initialized
-
-?    cout << ch;  <--  ch is not valid here. Its scope was for previous loop, which is over.
-
-?    for (char ch = 'a'; ch <= 'z'; ++ch);  //Correct. No ch variable already defined in this scope.
-
 ! Redefining variables (giving it a different value again), in general, is perfectly fine in C++.
 ! But we cannot declare it again (specify it's data type).
 */
