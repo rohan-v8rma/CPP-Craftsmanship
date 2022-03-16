4269cb503cae05dab8115276ad6b3cd180ad086e
rohan-verma19 <rv725555@gmail.com>
2022-03-16 17:07:45 +0530

    testing pointers as loop variables

--- rough.c
+++ rough.c
@@ -2,14 +2,19 @@
 
 int main()
 {
-    char hello[] = "abc";
-    hello[1] = 'y';
-    printf("%s\n", hello);
 
-    char *pointer = "abc";
-    //pointer[1] = 'y';  //this command will throw an error as pointer strings are immutable
-    printf("%s\n", pointer);
+    char str[128] = "We all scream for ice cream!";
+    char *pointer_1 = str;
+    // here we see again the loop exit condition *p == '\0'
+    while(*pointer_1) {
+        printf("%c", *pointer_1);
+        pointer_1++;
+    } 
+    printf("\n");
 
-    
+    for(char *pointer_2 = str; *pointer_2; pointer_2++) {
+        printf("%c", *pointer_2);
+    }
+    printf("\n");
     return 0;
 }
