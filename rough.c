be2568c43666a8e6882c1e3cf30a7833e1e4a8af
rohan-verma19 <rv725555@gmail.com>
2022-03-16 10:15:17 +0530

    testing pointer strings

--- rough.c
+++ rough.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main()
 {
-    int a = 165;
-    int b = 100;
-    float c = (float)(a/b);
-    printf("%f\n", c);
+    char hello[] = "abc";
+    hello[1] = 'y';
+    printf("%s\n", hello);
+
+    char *pointer = "abc";
+    //pointer[1] = 'y';  //this command will throw an error as pointer strings are immutable
+    printf("%s\n", pointer);
+
     
-    float d = (float)a/b;
-    printf("%f\n", d);
     return 0;
 }
