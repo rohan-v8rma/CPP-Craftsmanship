2da52cc96d5c24350dd5b036bf445bf840c4b3a4
rohan-verma19 <rv725555@gmail.com>
2022-03-22 08:41:41 +0530

    improv: appropriate file renaming

--- Functions/01Fu-c-FunctionParameters.cpp
+++ Functions/01Fu-c-FunctionParameters.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 /*
     INDEX
-1. Default Arguments in Functions
-2. Constant Arguments
+1. Actual and Formal Parameter
+2. Default Parameters in Functions
+3. Constant Parameters
 */
 
 /*
-? 1. Default Arguments in Functions
+* 2. Default Parameters in Functions
 
 We can assign default values to a function's parameter(s) which is useful in case a matching argument
 is not passed in the function call statement. The default values are specified  at the time of
@@ -16,35 +17,34 @@ function definition OR function prototyping.
 The default value of a parameter is used only when its matching argument is missing in the 'function call statement'.
 It sort of looks like variable initialization.
 
-* An important fact to know about default arguments is, any argument cannot have a default value
-* unless all arguments appearing on its right have their default values.
+? An important fact to know about default arguments is, any argument cannot have a default value
+? unless all arguments appearing on its right have their default values.
 
-! Example :
-
-    float interest(float principal, float time, float rate = 7.5) --> LEGAL
+? Example :
+?    float interest(float principal, float time, float rate = 7.5) --> LEGAL
 !   float interest(float principal, float time = 3, float rate) --> ILLEGAL    
 
 
-? 2. Constant Arguments
+* 3. Constant Parameters
 
-Constant Arguments means the function cannot modify these arguments. If we pass constant values to the function,
+Constant Parameters means the function cannot modify these arguments. If we pass constant values to the function,
 then the function cannot modify the values as the values are constants.
 
 For instance, the function call:
-! find_length("A string")
+? find_length("A string")
 
 cannot modify its argument "A string" as it is a constant value. 
 But what if the argument being passed is not a constant, it is in the form of a variable such as:
 
-! find_length(string)
+? find_length(string)
 
 where string may have any value. 
-* In order to make an argument constant to a function, we can use the keyword 'const' as show
 
+? In order to make an argument constant to a function, we can use the keyword 'const' as shown:
 ? int sum(const int a, const int b);
 
 The qualifier 'const' in function prototype tells the compiler that the function should not 
-modify the argument. Constant arguments are useful when functions are called by reference
+modify the argument. 
 */ 
 int main(){
     
