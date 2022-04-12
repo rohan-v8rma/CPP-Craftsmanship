c2d08ee763c2fb653580008694fa2dd70aa7a395
rohan-verma19 <rv725555@gmail.com>
2022-04-12 07:46:01 +0530

    feat: Adding two numbers by pass-by-reference

--- rough2.c
+++ rough2.c
@@ -1,22 +1,18 @@
 #include<stdio.h>
+#include<math.h>
+
+//Adding two numbers by pass by reference
+
+int addition(int* num_ptr_1, int* num_ptr_2) { //this function takes pointer arguments or address arguments
+    return (*num_ptr_1 + *num_ptr_2);   
+}
 
 int main() {
-    int integer_1, integer_2;
+    int num_1 = 1;
+    int num_2 = 2;
     
-    printf("Enter integer 1 : ");
-    scanf("%d", &integer_1);
-    
-    printf("\nEnter integer 2 : ");
-    scanf("%d", &integer_2);
-
-    int *pointer_1 = &integer_1;
-    int *pointer_2 = &integer_2;
-    if (*pointer_1 < *pointer_2) {
-        int temp = *pointer_1;
-        *pointer_1 = *pointer_2;
-        *pointer_2 = temp;
-    }
-    printf("The max out of %d and %d is %d", *pointer_1, *pointer_2, *pointer_1);
+    int sum = addition(&num_1, &num_2);
+    printf("%d", sum);
     
     return 0;
 }
