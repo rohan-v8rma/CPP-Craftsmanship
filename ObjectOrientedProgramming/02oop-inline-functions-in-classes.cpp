86d60b973752e9533bdb1cb9e8ebebc350bad920
Rohan Verma <rv725555@gmail.com>
2022-05-05 22:03:25 +0530

    feat: Class inline functions script added

---
+++ ObjectOrientedProgramming/02oop-inline-functions-in-classes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace::std;
+
+//TODO Read 14-inline-functions in Learning C repository for 
+
+/*
+Defining a member function within the class definition declares the function inline, 
+even if we do not use the inline specifier.
+*/
+
+int main() {
+
+}
