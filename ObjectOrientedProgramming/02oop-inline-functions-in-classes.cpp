01f8837cf8588fd4f365ae3e298e709efd20e5bd
Rohan Verma <rv725555@gmail.com>
2022-05-08 22:12:48 +0530

    improv: added inline function example

--- ObjectOrientedProgramming/02oop-inline-functions-in-classes.cpp
+++ ObjectOrientedProgramming/02oop-inline-functions-in-classes.cpp
@@ -7,7 +7,18 @@ using namespace::std;
 Defining a member function within the class definition declares the function inline, 
 even if we do not use the inline specifier.
 */
+class Point {
+    private:
+        int x, y;
+    public:
+        int getX() {
+            return x;
+        };
+        int getY() {
+            return y;
+        };
+};
 
-int main() {
-
-}
+/*
+Here, the functions getX() and getY() are inline member functions of the class `Point`
+*/
