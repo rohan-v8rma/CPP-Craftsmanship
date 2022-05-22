b6503b7ae98e95ac960b737150cdb65156a75872
Rohan Verma <rv725555@gmail.com>
2022-05-22 21:32:39 +0530

    improv: Explanation about return values of increment/decrement operators

--- Operators/01Op-Arithmetic.cpp
+++ Operators/01Op-Arithmetic.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
 using namespace std;
-/*
-    INDEX
-1. Arithmetic Operators
-    A. Binary Operators
-    B. Unary Operators
-2. Increment/Decrement Operators (Prefix and Postfix)
-*/
-int main(){
+
 /*
 * 1. Arithmetic Operators
 
@@ -32,71 +25,5 @@ Unary - : It precedes an operand as well. It reverses the sign of the operand's
 value.
 */
 
-
-/*
-* 2. Increment/Decrement Operators (++ and --)
-
-C++ includes 2 useful operators not generally found in other computer languages
-(except C). These are the increment(++) and decrement(--) operators. 
-
-The operator ++ adds 1 to its operand, and -- subtracts 1.
-
-In other words,
-a = a + 1 or a += 1 is the same as a++ or ++a
-
-a = a - 1 or a -= 1 is the same as a-- or --a
-
-The prefix and postfix version have the same effect on the operand but they
-differ when they take place in an expression.
-? Prefix version (++a): 
-In an expression, C++ performs the increment or decrement operation before 
-using the value of the operand to evaluate the expression.
-PRINCIPLE : Change-then-use
-*/
-    int sum = 8;
-    int prefix_count = 6;
-    int postfix_count = 6;
-    int prefix_final, postfix_final;
-
-    
-    prefix_final = sum * ++prefix_count;
-//In this case value of prefix_count swill be incremented before the multiplication
-//takes place. So the result will be 8 * (6+1) = 56    
-    
-    cout << prefix_final << '\n';
-
-/*
-? Postfix version(a++): 
-In an expression, C++ performs the increment or decrement operations after 
-using the value of the operand to evaluate the expression.
-PRINCIPLE : Use-then-change
-*/
-    postfix_final = sum * postfix_count++;
-//In this case the value of postfix_count will be incremented after the multiplication
-//takes place. So the result will be 8 * 6 = 48 after which value of postfix_count will change to 7
-    
-    cout << postfix_final << '\n';
-
-//In both the cases final value of the count will be same, only difference will be in value of expression
-
-/*
-! Important Case: b = a++ + ++a
-Such an expression, where value of a variable is being modified more than once,
-is UNDEFINED in C++. Some compilers won't give errors and give some result but no C++ rule
-defines their execution. It is completely compiler dependent.
-*/
-
-//Another Example
-    int n = 7;
-    cout << "n++ = " << n++ << endl;
-    cout << "n = " << n << endl;
-/*
-In this case as well, since n++ is written directly to be written to the output stream,
-C++ will first use the value of n(which is 7), write it to the output stream, then 
-increment n to 8.
-If we were to increment it beforehand or use ++n, we would have seen 8 as the first output.
-*/
-    cout << "\n\n";
-}
 
 
---
+++ Operators/02Op-Increment.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace::std;
+/*
+            INDEX
+* 1. Introduction about Increment/Decrement Operators (++ and --)
+* 2. Do increment expressions return values as well?
+    ? Prefix version (++a) 
+    ? Postfix version(a++)
+* 3. Modification of a variable more than once between two sequence points
+*/
+
+/*
+* 1. Introduction about Increment/Decrement Operators (++ and --)
+
+C++ includes 2 useful operators not generally found in other computer languages
+(except C). These are the increment(++) and decrement(--) operators. 
+
+The operator ++ adds 1 to its operand, and -- subtracts 1.
+
+In other words,
+a = a + 1 or a += 1 is the same as a++ or ++a
+
+a = a - 1 or a -= 1 is the same as a-- or --a
+
+The prefix and postfix version have the same effect on the operand but they
+differ when they take place in an expression.
+
+* 2. Do increment expressions return values as well?
+
+It is important to understand that expressions like (n++) or (++n), perform two tasks,
+? a. They increment/decrement the variable.
+? b. Return a value either before or after the increment/decrement operation.
+
+So if we assign an increment expression to a variable like:
+
+! variable = ++n;
+
+! So the expression on the right hand side will increment `n` first, then 
+! return the new value of `n`. As a result (n+1) gets assigned to `variable`.
+
+
+Now coming to the two types of operators:
+
+? Prefix version (++a): 
+
+In an expression, C++ performs the increment or decrement operation before 
+using the value of the operand to evaluate the expression.
+PRINCIPLE : Change-then-use
+*/
+int main() {
+    int sum = 8;
+    int prefix_count = 6;
+    int postfix_count = 6;
+    int prefix_final, postfix_final;
+
+    
+    prefix_final = sum * ++prefix_count;
+//In this case value of prefix_count swill be incremented before the multiplication
+//takes place. So the result will be 8 * (6+1) = 56    
+    
+    cout << prefix_final << '\n';
+
+/*
+? Postfix version(a++): 
+
+In an expression, C++ performs the increment or decrement operations after 
+using the value of the operand to evaluate the expression.
+PRINCIPLE : Use-then-change
+*/
+    postfix_final = sum * postfix_count++;
+//In this case the value of postfix_count will be incremented after the multiplication
+//takes place. So the result will be 8 * 6 = 48 after which value of postfix_count will change to 7
+    
+    cout << postfix_final << '\n';
+
+//In both the cases final value of the count will be same, only difference will be in value of expression
+
+/*
+* 3. Modification of a variable more than once between two sequence points
+
+! Important Case: b = a++ + ++a
+Such an expression, where value of a variable is being modified more than once,
+is UNDEFINED in C++. Some compilers won't give errors and give some result but no C++ rule
+defines their execution. It is completely compiler dependent.
+*/
+
+    cout << "\n\n";    
+    return 0;
+}
