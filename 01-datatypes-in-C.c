ed75a6d52aef2d708a28414c77763491468a7bf1
Rohan Verma <rv725555@gmail.com>
2023-04-12 20:54:36 +0530

    TODO: Move the content of these files into README.md

--- 01-datatypes-in-C.c
+++
@@ -1,7 +0,0 @@
-/*
-The datatypes in C are similar to those in C++
-except for the fact that C doesn't have 'string' datatype
-separately, instead a string in C is implemented as an 
-array of characters.
-*/
-
